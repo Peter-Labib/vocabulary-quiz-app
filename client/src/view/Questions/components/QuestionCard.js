e8328c0e04c863c2618975e9aa57309db233a5c0
Peter Labib <peter.adel.labib@gmail.com>
2023-06-17 04:30:12 +0300

    install uuid for keys

--- client/src/view/Questions/components/QuestionCard.js
+++ client/src/view/Questions/components/QuestionCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const options = ['noun', 'adverb', 'adjective', 'verb'];
 const QuestionCard = ({
@@ -12,10 +14,10 @@ const QuestionCard = ({
     <div>
       <p className='max-w-fit pt-6 pb-3'>{word}</p>
       <div className='flex space-x-3'>
-        {options.map((word, i) => (
+        {options.map((word) => (
           <button
             disabled={btnDisabled}
-            key={i}
+            key={uuidv4()}
             className={` p-2 rounded opacity-90 shadow hover:opacity-100 hover:shadow-lg transition-all ${
               selectedAnswer === word ? feedbackStyle : 'bg-slate-100'
             }`}
